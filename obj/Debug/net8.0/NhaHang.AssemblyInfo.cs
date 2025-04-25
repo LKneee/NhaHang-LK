814306a835aeb205d9c66f6477ba5e9a3921346a
Lê Khôi <lekhoi2208@gmail.com>
2025-04-26 02:25:22 +0700

    fix phieu order khi chon danh muc van k bi reset

--- obj/Debug/net8.0/NhaHang.AssemblyInfo.cs
+++ obj/Debug/net8.0/NhaHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NhaHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a948245f2ccb57f361440bd7e13b5d184c5c15a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea865b8274f082273cd1a88ac07fd4fad19f7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NhaHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NhaHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
