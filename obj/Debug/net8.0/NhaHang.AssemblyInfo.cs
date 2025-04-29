a3edb5672274ff7efec57bc74e76160a05730bdb
Lê Khôi <lekhoi2208@gmail.com>
2025-04-29 13:31:35 +0700

    xong chuc nang giam gia cho nvpv va an nut hoan tat khi don hang hoan tat

--- obj/Debug/net8.0/NhaHang.AssemblyInfo.cs
+++ obj/Debug/net8.0/NhaHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NhaHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814306a835aeb205d9c66f6477ba5e9a3921346a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f188579adf041dacb78d551fc47aa38692f1333")]
 [assembly: System.Reflection.AssemblyProductAttribute("NhaHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NhaHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
